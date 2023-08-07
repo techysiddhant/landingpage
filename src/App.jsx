7d17a397d46640db7d6fcd1e3a0e570f390485df
techysiddhant <sidjain8000@gmail.com>
2023-08-07 14:01:54 +0530

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,30 @@ import "./App.css";
 import Privacy from "./Pages/Privacy/Privacy";
 import Terms from "./Pages/Terms/Terms";
 import Home from "./Pages/Home/Home";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
-		<Routes>
-			<Route
-				exact
-				path="/"
-				element={<Home />}
-			/>
-			<Route
-				exact
-				path="/privacy-policy"
-				element={<Privacy />}
-			/>
-			<Route
-				exact
-				path="/termsandconditions"
-				element={<Terms />}
-			/>
-		</Routes>
+		// <Routes>
+		// 	<Route
+		// 		exact
+		// 		path="/"
+		// 		element={<Home />}
+		// 	/>
+		// 	<Route
+		// 		exact
+		// 		path="/privacy-policy"
+		// 		element={<Privacy />}
+		// 	/>
+		// 	<Route
+		// 		exact
+		// 		path="/termsandconditions"
+		// 		element={<Terms />}
+		// 	/>
+		// </Routes>
+		<div>
+			<Outlet></Outlet>
+		</div>
 	);
 }
 
